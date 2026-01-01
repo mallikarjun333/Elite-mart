bc35e111db720d08ff7dbf3e2766868f721563ba
Mallikarjuna Char A S <mallikarjun132000@gmail.com>
2026-01-01 16:57:04 +0530

    wfnb

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+  function toggleMenu() {
+  document.getElementById("sideMenu").classList.add("open");
+  document.getElementById("overlay").classList.add("open");
+}
+
+function closeMenu() {
+  document.getElementById("sideMenu").classList.remove("open");
+  document.getElementById("overlay").classList.remove("open");
+}
